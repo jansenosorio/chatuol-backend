abb88471a13b2958c65d27470c7658771da9964e
JansenOsorio <dev.jansen93@gmail.com>
2023-01-16 08:24:03 -0300

    fix: conection to server

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
-import express from "express";
-import { MongoClient } from "mongodb";
+import express from 'express'
+import cors from 'cors'
+import { MongoClient } from 'mongodb'
 import dotenv from 'dotenv'
-
+import dayjs from 'dayjs'
+import joi from 'joi'
 dotenv.config()
 
-const app = express()
-app.use(express.json())
+const server = express()
+server.use(cors())
+server.use(express.json())
 
-const mongoClient = new MongoClient(process.env.MONGO_URL)
-let db
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
 
-mongoClient.connect().then(() => {
-  db = mongoClient.db("test")
-})
+try {
+
+    await mongoClient.connect()
+    console.log('OK, Servidor funcionando.')
+
+} catch (err) {
+    console.log("Houve algum erro e não foi possível se conectar.")
+}
+
+const db = mongoClient.db()
